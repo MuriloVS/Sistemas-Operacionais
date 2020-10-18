9592186ced56c07e4dbf3e3cd7a63ee61754b60b
MuriloVS <40902924+MuriloVS@users.noreply.github.com>
2020-10-18 16:38:06 -0300

    Versão "final" com Tasks

--- SomaVetoresThreads/Program.cs
+++ SomaVetoresThreads/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,33 +8,29 @@ namespace SomaVetoresThreads
 {
     class Program
     {
-        private static long total = 0;
-        private static long[] v1;
-        private static long[] v2;
-
+        static long total = 0;
+       
         static void Main(string[] args)
         {
             Console.WriteLine("Tamanho do vetor: ");
-            long tamanho = int.Parse(Console.ReadLine());
+            long tamanho = long.Parse(Console.ReadLine());
+            // long tamanho = 100_000_000;
 
-            v1 = new long[tamanho];
-            v2 = new long[tamanho];
+            long[] v1 = new long[tamanho];
+            long[] v2 = new long[tamanho];
 
             Random rnd = new Random();
 
-            // preenchendo os vetores com números randômicos
-            // e calculando o total para comparar com o resultado das threads
-            long teste = 0;
-            for (int x = 0; x < tamanho; x++)
+            // preenchendo os vetores com números randômicos (1 - 10)           
+            for (long x = 0; x < tamanho; x++)
             {
                 v1[x] = rnd.Next(11);
                 v2[x] = rnd.Next(11);
-                teste += v1[x] + v2[x];
             }
             
             Console.WriteLine("Informe o número de threads: ");
-            //int threads = int.Parse(Console.ReadLine());
-            int threads = Environment.ProcessorCount;
+            int threads = int.Parse(Console.ReadLine());
+            // int threads = Environment.ProcessorCount;
 
             // qual o tamanho de cada divisão do vetor original
             long diferenca = tamanho / threads;
@@ -47,30 +45,36 @@ namespace SomaVetoresThreads
 
             Task[] tarefas = new Task[threads];
             long inicio, fim;
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
             for (int x = 0; x < threads; x++)
             {
-                // quando passamos os limites diretamente temos erro de índice
+                // quando passamos os limites diretamente podemos ter erro de índice
+                // porque funções lambda pegam uma referência à variável
                 inicio = limite[x];
                 fim = limite[x + 1];
-                tarefas[x] = Task.Run(() => Soma(v1, v2, inicio, fim));
+                tarefas[x] = Task.Run(() => SomaVetores(v1, v2, inicio, fim));
                 // sem esse wait temos erro nas somas - não sei o porquê...
                 tarefas[x].Wait(1);
             }
 
-            // "garante" que todas as threads vão terminar antes do programa sair
+            // "garante" que todas as tarefas vão ser terminadas
             Task.WaitAll(tarefas);
+            sw.Stop();
 
-            Console.WriteLine($"\n\nteste = { teste }");
-            Console.WriteLine($"total = { total }");
+            Console.WriteLine($"\n\nteste = { v1.Sum() + v2.Sum() }");
+            Console.WriteLine($"total = { total } - { sw.ElapsedMilliseconds }ms");
         }
 
-        private static void Soma(long[] v1, long[] v2, long start, long end)
+        private static void SomaVetores(long[] v1, long[] v2, long inicio, long fim)
         {            
-            for (long y = start; y < end; y++)
+            for (long x = inicio; x < fim; x++)
             {
-                // todas as threads modificam soma
+                // todas as threads/tarefas modificam soma
                 // é necessário garantir que elas não o façam ao mesmo tempo
-                Interlocked.Add(ref total, v1[y] + v2[y]);
+                Interlocked.Add(ref total, v1[x] + v2[x]);
                 // Console.Write($"Thread={Thread.CurrentThread.ManagedThreadId} ");
             }
         }
